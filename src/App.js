d5b22512e0952188bf57257deeab798e54d36dd2
Virginia Morilla <virginia.morilla@gmail.com>
2021-12-28 20:13:25 +0100

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Tabla from './components/TablaAlumnos/Tabla';
+
+function App() {
+  return (
+    <div className="App">
+     <Tabla />
+    </div>
+  );
+}
+
+export default App;
